b8a877caf642bddaaea2a8eaeedbc8379d6025d7
Moses <73477488+ThutMoses@users.noreply.github.com>
2021-02-25 20:28:09 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,8 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-
+<head><script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+</head>
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
